a8a0f63bdfb0e0212d62b5e42ee76ef49d0b8868
Kyle <foscat4o1k@outlook.com>
2018-12-09 14:09:48 -0600

    starting to sync custom js to project js and db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,25 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
+  // Get all examples **still needs work**--only attached our db name into function
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+    db.Example.findAll({}).then(function(ex_db) {
+      res.json(ex_db);
     });
   });
 
-  // Create a new example
+  // Create a new example **still needs work**--only attached our db name into function
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.create(req.body).then(function(ex_db) {
+      res.json(ex_db);
     });
   });
 
-  // Delete an example by id
+  // Delete an example by id **still needs work**--only attached our db name into function
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(ex_db) {
+      res.json(ex_db);
     });
   });
 };
+
