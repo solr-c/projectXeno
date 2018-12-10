76f5367c7bdb5f0f6501b8b39836518bacd1d4ab
Sapulsic <carlos.barahona1995@gmail.com>
2018-12-10 01:52:37 -0600

    passport user auth

--- models/user.js
+++ models/user.js
@@ -1,50 +1,50 @@
 module.exports = function(sequelize, Sequelize) {
 var User = sequelize.define("user", {
-id: {
-    autoIncrement: true,
-    primaryKey: true,
-    type: Sequelize.INTEGER
-},
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
 
-firstname: {
-    type: Sequelize.STRING,
-    notEmpty: true
-},
+    firstname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
 
-lastname: {
-    type: Sequelize.STRING,
-    notEmpty: true
-},
+    lastname: {
+        type: Sequelize.STRING,
+        notEmpty: true
+    },
 
-username: {
-    type: Sequelize.TEXT
-},
+    username: {
+        type: Sequelize.TEXT
+    },
 
-about: {
-    type: Sequelize.TEXT
-},
+    about: {
+        type: Sequelize.TEXT
+    },
 
-email: {
-    type: Sequelize.STRING,
-    validate: {
-    isEmail: true
+    email: {
+        type: Sequelize.STRING,
+        validate: {
+        isEmail: true
+        }
+    },
+
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    last_login: {
+        type: Sequelize.DATE
+    },
+
+    status: {
+    type: Sequelize.ENUM("active", "inactive"),
+    defaultValue: "active"
     }
-},
-
-password: {
-    type: Sequelize.STRING,
-    allowNull: false
-},
-
-last_login: {
-    type: Sequelize.DATE
-},
-
-status: {
-type: Sequelize.ENUM("active", "inactive"),
-defaultValue: "active"
-}
 });
 
-return User;
+    return User;
 };
