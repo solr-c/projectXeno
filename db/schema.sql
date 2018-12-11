55d3fb363f7c88a901133bc4834443bc4c9c9003
Sapulsic <carlos.barahona1995@gmail.com>
2018-12-11 14:25:07 -0600

    DB folder and schema

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS ex_db;
+CREATE DATABASE ex_db;
+
+USE ex_db;
+
+CREATE TABLE user (
+	id INT NOT NULL AUTO_INCREMENT,
+	firstname VARCHAR(100) NULL,
+	lastname VARCHAR(100) NULL,
+	username VARCHAR(100) NULL,
+	email VARCHAR(100) NULL,
+    password VARCHAR(100) NULL,
+	PRIMARY KEY(id)
+);
